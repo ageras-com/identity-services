7fb1b64ea789c506614ad167607a90735143f70b
Adam Kvist Lamaa <kvistlamaa@outlook.com>
2025-04-29 15:17:21 +0200

    Testing prettier on commit

--- apps/portal/src/lib/helper.ts
+++ apps/portal/src/lib/helper.ts
@@ -1,4 +1,4 @@
 export const sampleFunction = () => {
   // Sample function to demonstrate code completion
-    console.log("This is a sample function")
+        console.logs("This is a sample function")
 }
