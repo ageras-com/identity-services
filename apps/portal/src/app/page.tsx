fdecd841907bd3e8a7e04aa0e40ac305115ddc33
Adam Kvist Lamaa <kvistlamaa@outlook.com>
2025-04-29 14:40:41 +0200

    Testing husky 2

--- apps/portal/src/app/page.tsx
+++ apps/portal/src/app/page.tsx
@@ -95,7 +95,7 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Go to NEXTJS.org →
+          Go to nextjs.org →
         </a>
       </footer>
     </div>
