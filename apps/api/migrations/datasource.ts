5e84742973894b9a093e88ac5da2c8530561d657
lrbh10 <rabah.laouadi@shine.fr>
2025-07-08 15:45:22 +0200

    feat(migration): make migration working

--- apps/api/migrations/datasource.ts
+++ apps/api/migrations/datasource.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { InfrastructureModule } from '../src/infrastructure/infrastructure.module';
 import { DataSource } from 'typeorm';
+import { TypedConfigService } from '../src/infrastructure/config/typed-config.service';
+import { dataSourceOptions } from '../src/infrastructure/typeorm/typeorm';
+import migrations from '../migrations';
 
 export default NestFactory.create(InfrastructureModule)
-  .then((app) => app.get(DataSource))
-  .then((dataSource) => Promise.all([dataSource, dataSource.destroy()]))
-  .then(([dataSource]) => dataSource);
+  .then((app) => app.get(TypedConfigService))
+  .then((configService) => {
+    const datasourceOptionsConfig = dataSourceOptions(configService);
+
+    return new DataSource({
+      ...datasourceOptionsConfig,
+      migrations,
+    });
+  })
+  .then((dataSource) => dataSource);
